aba10650ba7bd1293a312ead22a8b165cc9b8782
Kirill Tkachev <tkachev89@mail.ru>
2026-04-19 15:15:39 +0300

    fix lint

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -5,10 +5,10 @@ DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 def is_prime(n):
     if n < 2:  
-        return False  
+        return False 
     for i in range(2, n):  
         if n % i == 0:  
-            return False  
+            return False 
     return True
 
 
