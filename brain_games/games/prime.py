4876732a2ab5598f482cb47f8f69acb455efa233
Kirill Tkachev <tkachev89@mail.ru>
2026-04-19 15:08:26 +0300

    Добавлена игра "Простое ли число?"

---
+++ brain_games/games/prime.py
@@ -0,0 +1,24 @@
+import random
+
+DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+
+
+def is_prime(n):
+    if n < 2:  
+        return False  
+    for i in range(2, n):  
+        if n % i == 0:  
+            return False  
+    return True
+
+
+def generate_expression():
+    number = random.randint(1, 100)
+    question = str(number)
+
+    if is_prime(number):
+        correct_answer = "yes"
+    else:
+        correct_answer = "no"
+
+    return question, correct_answer
---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,10 @@
+import brain_games.games.prime
+from brain_games.engine import run_game
+
+
+def main():
+    run_game(brain_games.games.prime)
+    
+
+if __name__ == "__main__":
+    main()
